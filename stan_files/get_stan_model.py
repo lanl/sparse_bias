e2d86e69726fbddd0423be556ddf15f0a811505e
nwalton <nwalton@lanl.gov>
2025-02-12 08:33:06 -0700

    relative path update

--- stan_files/get_stan_model.py
+++ stan_files/get_stan_model.py
@@ -1,11 +1,12 @@
-stan_files_path = "/Users/mikegros/Projects/AIACHNE/sparse_bias/stan_files"
+import os
+stan_files_path = os.path.dirname(__file__)
 
 model_paths= {
-    "interpolation_horseshoe":      stan_files_path + "interpolatedmean_normtau_fitds.stan",
-    "interpolation_horseshoe_corr": stan_files_path + "interpolatedmean_normtau_fitds_corr.stan",
-    "maxwellian_horseshoe":         stan_files_path + "maxwellian_normtau_corr.stan", 
-    "maxwellian_noBias":            stan_files_path + "maxwellian_noBias.stan",    
-    "maxwellian_noHorseshoe":       stan_files_path + "maxwellian_noHorseshoe.stan"    
+    "interpolation_horseshoe":      os.path.join(stan_files_path, "interpolatedmean_normtau_fitds.stan") ,
+    "interpolation_horseshoe_corr": os.path.join(stan_files_path, "interpolatedmean_normtau_fitds_corr.stan") ,
+    "maxwellian_horseshoe":         os.path.join(stan_files_path, "maxwellian_normtau_corr.stan") , 
+    "maxwellian_noBias":            os.path.join(stan_files_path, "maxwellian_noBias.stan") ,    
+    "maxwellian_noHorseshoe":       os.path.join(stan_files_path, "maxwellian_noHorseshoe.stan")     
 }
 
 def get_stan_model(model_string):
