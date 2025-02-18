3263ca0cad6a4b79a4e755cd589dfc476aa36999
nwalton <nwalton@lanl.gov>
2025-02-18 11:05:30 -0700

    Dont overwrite fit method with output attribute

--- sparse_bias.py
+++ sparse_bias.py
@@ -33,7 +33,7 @@ class BiasModel:
         self.stan_data   = 1
 
     def fit(self, n_warmup=2000, n_sample=2500, **kwargs):
-        self.fit = self.model.sample(data=self.data_dict,
+        self.output = self.model.sample(data=self.data_dict,
                                      iter_warmup   = n_warmup, 
                                      iter_sampling = n_sample,
                                      **kwargs)      
